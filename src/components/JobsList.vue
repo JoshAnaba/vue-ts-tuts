7ac119b8f6148860911367e4ac5493cd170f6176
joshanaba <anabajoshchi@gmail.com>
2022-08-15 00:20:40 +0100

    vue ts tutorial

---
+++ src/components/JobsList.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="job-list">
+    <p>Ordered by {{order}}</p>
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+      <h2>{{job.title}} in {{job.location}}</h2>
+      <div class="salary">
+        <p>{{job.salary}} dollars</p>
+      </div>
+      <div class="description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nostrum perspiciatis eius illo inventore voluptates alias consequuntur provident nisi laborum, porro dolores atque animi eaque reiciendis? Nostrum officiis fugiat porro!
+        </p>
+      </div>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Job from '@/types/Job'
+import OrderTerm from '@/types/OrderTerm'
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>
+    }
+  }
+})
+</script>
+
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <div> -->
+    <div class="order-by">
+      <p>Order by</p>
+      <button @click="handleClick('salary')">Salary</button>
+      <button @click="handleClick('title')">Title</button>
+      <button @click="handleClick('location')">Location</button>
+    </div>
+    <JobsList :jobs="jobs" :order="order" />
+    <!-- </div> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { defineComponent, reactive, toRefs, ref } from "vue";
+import JobsList from "@/components/JobsList.vue";
+import Job from "@/types/Job";
+import OrderTerm from "@/types/OrderTerm";
 export default defineComponent({
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld,
+    JobsList,
+  },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "Farm worker",
+        location: "Lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "Software Devr",
+        location: "LDN",
+        salary: 300000,
+        id: "2",
+      },
+      {
+        title: "Pastor",
+        location: "Kenya",
+        salary: 20000,
+        id: "3",
+      },
+      {
+        title: "Laundryman",
+        location: "America",
+        salary: 10000,
+        id: "4",
+      },
+      {
+        title: "Artiste",
+        location: "Nigeria",
+        salary: 34000,
+        id: "5",
+      },
+      {
+        title: "Bus Driver",
+        location: "China",
+        salary: 1000,
+        id: "6",
+      },
+    ]);
+    const order = ref<OrderTerm>('title')
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+    return {
+      jobs,
+      handleClick,
+      order
+    };
   },
 });
 </script>
+
+<style scoped>
+.order-by button {
+  margin-right: 10px;
+}
+</style>
